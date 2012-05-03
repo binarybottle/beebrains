62282dc3c5128c6938903b8a32a4a173f7426928
Arno Klein <arno@binarybottle.com>
2012-05-02 23:33:04 -0400

    revamped process_beebrains.py after bthirion bug fix

--- process_beebrains.py
+++ process_beebrains.py
@@ -3,7 +3,9 @@
 """
 Processing steps:
 
-(1) 
+(1) Compute a design matrix
+(2) Apply a GLM to all voxels
+(3) Create a contrast image
 
 Outputs: 
 
@@ -23,131 +25,57 @@ https://github.com/nipy/nipy/blob/master/examples/labs/example_glm.py
 """
 
 from nipy.modalities.fmri.design_matrix import make_dmtx
+from nipy.modalities.fmri.experimental_paradigm import BlockParadigm
+import numpy as np
+import pylab as mp
+import os.path as op
+import nibabel as nib
+import nipy.labs.glm as GLM
 
-# http://neuroimaging.scipy.org/doc/manual/html/api/generated/nipy.neurospin.utils.design_matrix.html
-# class nipy.neurospin.utils.design_matrix.DesignMatrix(frametimes=None, paradigm=None, hrf_model='Canonical', drift_model='Cosine', hfcut=128, drift_order=1, fir_delays=[, 0], fir_duration=1.0, cond_ids=None, add_regs=None, add_reg_names=None)
+ANTS = '/software/ANTS_1.9/bin/'  # for registration (motion correction)
 
-paradigm = BlockParadigm(con_id=['awake','odor','odor','odor',...], onset=[0, 5], duration=[10, 2], amplitude=[1, 0.2, 0.3])
+xdim = 130  # x dimension for each image
+ydim = 172  # y dimension for each image
+frames_per_run = 232  # number of images captured for each run
 
-dmtx = make_dmtx(np.linspace(0,19,20), paradigm, hrf_model='FIR', drift_model='Blank', hfcut=np.inf)
+########################################
+# Construct a design matrix
+########################################
+
+frametimes = np.linspace(0,2319,2320)
+contrasts = ['awake', 'odor','odor','odor','odor','odor','odor','odor','odor']
+onsets = [1160, 233,465,697,929,1393,1625,1857,2089]
+durations = [1160, 232,232,232,232,232,232,232,232]
+amplitudes = [1, 0.6,0.4,0.3,0.2,0.6,0.4,0.3,0.2]
+paradigm = BlockParadigm(con_id=contrasts, onset=onsets, duration=durations, amplitude=amplitudes)
+
+dmtx = make_dmtx(frametimes, paradigm, hrf_model='FIR', drift_model='Blank', hfcut=np.inf)
 
 dmtx.matrix
 
-
-"""
-Examples of design matrices specification and and computation
-(event-related design, FIR design, etc)
-
-Author : Bertrand Thirion: 2009-2010
-https://github.com/nipy/nipy/blob/master/examples/labs/demo_dmtx.py
-"""
-import numpy as np
-import pylab as mp
-from nipy.modalities.fmri.design_matrix import make_dmtx
-from nipy.modalities.fmri.experimental_paradigm import \
-    EventRelatedParadigm, BlockParadigm
-
-# frame times
-tr = 1.0
-nscans = 128
-frametimes = np.linspace(0, (nscans - 1) * tr, nscans)
-
-# experimental paradigm
-conditions = ['c0', 'c0', 'c0', 'c1', 'c1', 'c1', 'c3', 'c3', 'c3']
-onsets = [30, 70, 100, 10, 30, 90, 30, 40, 60]
-hrf_model = 'Canonical'
-motion = np.cumsum(np.random.randn(128, 6), 0)
-add_reg_names = ['tx', 'ty', 'tz', 'rx', 'ry', 'rz']
-
-# block design matrix
-duration = 7 * np.ones(9)
-paradigm = BlockParadigm(con_id=conditions, onset=onsets,
-                             duration=duration)
-
-X2 = make_dmtx(frametimes, paradigm, drift_model='Polynomial',
-                         drift_order=3)
 # plot the results
-fig = mp.figure(figsize=(10, 6))
-ax = mp.subplot(1, 3, 1)
-X2.show(ax=ax)
-ax.set_title('Block design matrix', fontsize=12)
-ax = mp.subplot(1, 3, 3)
-
-
-"""
-This is an example where
-1. An sequence of fMRI volumes are simulated
-2. A design matrix describing all the effects related to the data is computed
-3. A GLM is applied to all voxels
-4. A contrast image is created
-
-Author : Bertrand Thirion, 2010
-https://github.com/nipy/nipy/blob/master/examples/labs/example_glm.py
-"""
-print __doc__
-
-import numpy as np
-import os.path as op
-import tempfile
-
-from nibabel import save, Nifti1Image
-import nipy.modalities.fmri.design_matrix as dm
-from nipy.labs.utils.simul_multisubject_fmri_dataset import \
-     surrogate_4d_dataset
-import nipy.labs.glm as GLM
-from nipy.modalities.fmri.experimental_paradigm import EventRelatedParadigm
-
-#######################################
-# Simulation parameters
-#######################################
-
-# volume mask
-shape = (20, 20, 20)
-affine = np.eye(4)
-
-# timing
-n_scans = 128
-tr = 2.4
-
-# paradigm
-frametimes = np.linspace(0, (n_scans - 1) * tr, n_scans)
-conditions = np.arange(20) % 2
-onsets = np.linspace(5, (n_scans - 1) * tr - 10, 20) # in seconds
-hrf_model = 'Canonical'
-motion = np.cumsum(np.random.randn(n_scans, 6), 0)
-add_reg_names = ['tx', 'ty', 'tz', 'rx', 'ry', 'rz']
-
-# write directory
-swd = tempfile.mkdtemp()
-
-########################################
-# Design matrix
-########################################
-
-paradigm = EventRelatedParadigm(conditions, onsets)
-X, names = dm.dmtx_light(frametimes, paradigm, drift_model='Cosine', hfcut=128,
-                         hrf_model=hrf_model, add_regs=motion,
-                         add_reg_names=add_reg_names)
-
-
-#######################################
-# Get the FMRI data
-#######################################
-
-fmri_data = surrogate_4d_dataset(shape=shape, n_scans=n_scans)[0]
-
-# if you want to save it as an image
-data_file = op.join(swd, 'fmri_data.nii')
-save(fmri_data, data_file)
+mp.figure(figsize=(10, 6))
+dmtx.show()
+mp.title('Block design matrix')
+mp.show()
 
 ########################################
 # Perform a GLM analysis
 ########################################
 
+# Load data
+inputvolumes = 'output/bk120309e.lst_smoothed/image*.nii.gz'
+outputvolume = 'output/bk120309e.lst_smoothed.nii.gz'
+
+cmd = [ANTS+'StackSlices', outputvolume, str(xdim), str(ydim), str(frames_per_run), inputvolumes]
+print(' '.join(cmd)); os.system(' '.join(cmd))                
+
+img = nib.load(outputvolume)
+Y = img.get_data()
+
 # GLM fit
-Y = fmri_data.get_data()
-model = "ar1"
 method = "kalman"
+model = "ar1"
 glm = GLM.glm()
 glm.fit(Y.T, X, method=method, model=model)
 
@@ -157,20 +85,19 @@ contrast[0] = 1
 contrast[1] = - 1
 my_contrast = glm.contrast(contrast)
 
-# compute the constrast image related to it
+# compute the contrast image related to it
 zvals = my_contrast.zscore()
-contrast_image = Nifti1Image(np.reshape(zvals, shape), affine)
+contrast_image = nib.Nifti1Image(np.reshape(zvals, shape), affine)
 
 # if you want to save the contrast as an image
 contrast_path = op.join(swd, 'zmap.nii')
-save(contrast_image, contrast_path)
+nib.save(contrast_image, contrast_path)
 
-
-print 'wrote the some of the results as images in directory %s' % swd
+print 'wrote some of the results as images in directory %s' % swd
 
 h, c = np.histogram(zvals, 100)
-import pylab
-pylab.figure()
-pylab.plot(c[: - 1], h)
-pylab.title(' Histogram of the z-values')
-pylab.show()
+mp.figure()
+mp.plot(c[: - 1], h)
+mp.title('Histogram of the z-values')
+mp.show()
+
